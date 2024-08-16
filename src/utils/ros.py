53fdeac37279e66a6e959c925efa0ea18db5ef3b
jason <jasonah@seas.upenn.edu>
2024-08-16 11:54:58 -0400

    update detection header

--- src/utils/ros.py
+++ src/utils/ros.py
@@ -23,8 +23,7 @@ def create_detection_msg(img_msg: Image, detections: torch.Tensor) -> Detection2
     detection_array_msg = Detection2DArray()
 
     # header
-    header = create_header()
-    detection_array_msg.header = header
+    detection_array_msg.header = img_msg.header
     for detection in detections:
         x1, y1, x2, y2, conf, cls = detection.tolist()
         single_detection_msg = Detection2D()
